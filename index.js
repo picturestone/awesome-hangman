cfaaa2db34d3dd5af99976a8217bb0373c262374
Lukas Bildstein <lukas.bildstein@massiveart.com>
2022-05-17 18:54:51 +0200

    Added letter frequency

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('hello world');
+
+const occuranaceArray = [
+    'E', 'S', 'I', 'A', 'R', 'N', 'T', 'O', 'L', 'C', 'D', 'U', 'G', 'P', 'M', 'H', 'B', 'Y', 'F', 'V', 'K', 'W', 'Z', 'X', 'J', 'Q'
+];
+
