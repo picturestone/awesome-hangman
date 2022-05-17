30c72a263a10999db24326f4334f48444fbf5b4f
Daniel Domberger <ddo@foryouandyourcustomers.com>
2022-05-17 19:37:46 +0200

    implement submit & guess

--- api.mjs
+++ api.mjs
@@ -1,11 +1,22 @@
 import fetch from 'node-fetch'
 
+const token = "RJHUJS";
 export const status = () => {
-// const Bearer: <token>
-    const token = "RJHUJS";
-   return fetch('https://trayport-hangman.azurewebsites.net/api/status', {
+    return fetch('https://trayport-hangman.azurewebsites.net/api/status', {
         headers: {Authorization: 'Bearer RJHUJS'}
     })
 }
 
-// export default status;
+export const guess = (id, char) => {
+    return fetch(`https://trayport-hangman.azurewebsites.net/api/guess/${id}/${char}`, {
+        method: "PUT",
+        headers: {Authorization: 'Bearer RJHUJS'}
+    })
+}
+
+export const submit = (id, word) => {
+    return fetch(`https://trayport-hangman.azurewebsites.net/api/submit/${id}/${word}`, {
+        method: "POST",
+        headers: {Authorization: 'Bearer RJHUJS'}
+    })
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {status} from "./api.mjs";
+import {guess, status, submit} from "./api.mjs";
 
 console.log('hello world');
 
@@ -6,12 +6,19 @@ const occuranaceArray = [
     'E', 'S', 'I', 'A', 'R', 'N', 'T', 'O', 'L', 'C', 'D', 'U', 'G', 'P', 'M', 'H', 'B', 'Y', 'F', 'V', 'K', 'W', 'Z', 'X', 'J', 'Q'
 ];
 
-
 const main = async () => {
     try {
         const response = await status()
         console.log(response.status);
         console.log(await response.json())
+
+        const res2 = await guess(0, 'e')
+        console.log(res2.status);
+        console.log(await res2.json())
+
+        const res3 = await submit(0, 'first')
+        console.log("POST: " + res3.status);
+        console.log(await res3.json())
     } catch (e) {
         console.error(e)
     }
